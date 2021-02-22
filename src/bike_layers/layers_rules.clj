1370b92d948044a7fda3fda081433c7d488080d3
Heather Farley <heather@mayvenn.com>
2021-02-22 09:09:38 -0800

    Rules and tests for head and neck gear

--- src/bike_layers/layers_rules.clj
+++ src/bike_layers/layers_rules.clj
@@ -6,15 +6,13 @@
 ;; Heac Gear addons: Ear muffs, Cycowl, yellow/gray scarf
 
 ;; Torso: red reflective jacket, blue reflective jacket, non-bike heavier coat, bright shawl
-;; Torse addons: poncho
-
-
+;; Torse addons: rain-poncho
 
 ;; helmet/neck rules:
 ;; if it's < 50° F, cycowl and beanie
 ;; if it's 50 <= X < 60, ear muffs, scarf
 ;; if it's 60 <= X < 70,  ear muffs, jacket
-;; if it's 70 <= X < 80,  bright shawl, fancy hat
+;; if it's 70 <= X < 80,  bright shawl, straw hat
 ;; if it's 70 <= X < 80, straw hat
 ;; if it's 80 <= X, straw hat
 
@@ -28,12 +26,9 @@
 ;; only 1 torso warm thing at a time: non-bike heavier coat, blue jacket, red jacket, bright shawl
 
 ;;; Things to check: precipitation, temperature, daylight (later- get this going first)
+;; should I add the new orange poncho to this list?
 
 (defrecord Weather [temperature precipitation])
-
-;; (defn make-weather [conditions]
-;;   (let [default-conditions {:temp 70 :rain 0}]
-;;     (map->Weather (merge default-conditions conditions))))
 
 (defrecord HeadGear [helmet head-addons])
 
@@ -45,18 +40,36 @@
   =>
   (insert! (->HeadGear :beanie [:cycowl])))
 
-;; (defrule temp-between-50-and-60-wear
-;;   "If it's cold out, wear a warm helmet, that's what it's for."
-;;   [Weather (< temperature 50)]
-;;   =>
-;;   (insert! (->HeadGear :beanie [:cycowl])))
+(defrule between-50-and-60-wear-earmuffs-and-scarf
+  "If it's chilly out, keep your neck and ears warm"
+  [Weather (and (>= temperature 50) (< temperature 60))]
+  =>
+  (insert! (->HeadGear :regular [:earmuffs :scarf])))
+
+(defrule between-60-and-70-wear-earmuffs
+  "If it's cool out, your ears may be nippy"
+  [Weather (and (>= temperature 60) (< temperature 70))]
+  =>
+  (insert! (->HeadGear :regular [:earmuffs])))
+
+(defrule between-70-and-80-wear-a-fancy-hat
+  "If it's warm, you might still get a little breeze, so wear a nice shawl"
+  [Weather (and (>= temperature 70) (< temperature 80))]
+  =>
+  (insert! (->HeadGear :straw-hat [:bright-shawl])))
+
+(defrule over-80-keep-the-sun-out-of-your-eyes
+  "The sun is bright and right in your eyes; wear a straw hat"
+  [Weather (and (>= temperature 80))]
+  =>
+  (insert! (->HeadGear :straw-hat [])))
 
 (defquery headgear?
   "Returns what to wear on my head"
   []
   [HeadGear (= ?helmet helmet) (= ?head-addons head-addons)])
 
-(comment 
+(comment
   (println (-> (mk-session)
                (insert (->Weather 45 0))
                (fire-rules)
--- test/bike_layers/layers_rules_test.clj
+++ test/bike_layers/layers_rules_test.clj
@@ -4,15 +4,38 @@
             [clara.rules :refer :all]))
 
 (defn test-session
-  [weather test-query]
+  [weather]
   (-> (mk-session 'bike-layers.layers-rules)
       (insert weather)
-      (fire-rules)
-      (query test-query)))
+      (fire-rules)))
 
-(deftest temperature-affects-helmet-and-neck-gear-suggestions
+(deftest temperature-affects-gear-suggestions
   (testing "when it is colder than 50° F, I should wear the cycowl and beanie helmet"
     (let [weather (->Weather 40 0)
-          [result]  (test-session weather headgear?)]
+          [result]  (query (test-session weather) headgear?)]
       (is (= (:?helmet result) :beanie))
-      (is (= (:?head-addons result) [:cycowl])))))
+      (is (= (:?head-addons result) [:cycowl]))))
+
+  (testing "when it is warmer than 50° F and colder than 60°, I should wear earmuffs and a scarf"
+    (let [weather (->Weather 55 0)
+          [result]  (query (test-session weather) headgear?)]
+      (is (= (:?helmet result) :regular))
+      (is (= (:?head-addons result) [:earmuffs :scarf]))))
+
+  (testing "when it is warmer than 60° F and colder than 70°, I should wear earmuffs"
+    (let [weather (->Weather 65 0)
+          [result]  (query (test-session weather) headgear?)]
+      (is (= (:?helmet result) :regular))
+      (is (= (:?head-addons result) [:earmuffs]))))
+
+  (testing "when it is warmer than 70° F and colder than 80°, I should wear a bright shawl and straw hat"
+    (let [weather (->Weather 75 0)
+          [result]  (query (test-session weather) headgear?)]
+      (is (= (:?helmet result) :straw-hat))
+      (is (= (:?head-addons result) [:bright-shawl]))))
+
+  (testing "when it is warmer than 80° F, keep the sun out of your eyes"
+    (let [weather (->Weather 85 0)
+          [result]  (query (test-session weather) headgear?)]
+      (is (= (:?helmet result) :straw-hat))
+      (is (= (:?head-addons result) [])))))
